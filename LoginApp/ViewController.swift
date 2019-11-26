fcdec089dbd62699cddccf614d28b99e9b75ebdb
zrow32 <z.rows32@gmail.com>
2019-11-26 11:24:13 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Vladimir Zhirov on 11/26/19.
+//  Copyright © 2019 Vladimir Zhirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
